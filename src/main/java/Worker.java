3124c79e6a7418aa372c78e0610598ee91293983
michpalm <michpalm95@gmail.com>
2017-05-18 15:24:35 +0200

    Fully functional TTR!!
    Ratio-Content pair added as a class.
    Comments need fixing and some code clean up!

--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -54,7 +54,7 @@ public class Worker implements Runnable { //(added implements runnable)
     ArrayList <Integer> coffA;
     ArrayList <Integer> coffB;
     String link;
-    int pageCount;
+    
     public Worker( ArrayList<Integer> CoffA, ArrayList<Integer> CoffB, String link){
             this.coffA=CoffA;
             this.coffB=CoffB;
@@ -68,23 +68,20 @@ public class Worker implements Runnable { //(added implements runnable)
         long startTime=System.currentTimeMillis();
         ArrayList<String> domainLinks = new ArrayList<String>();
 
-        // Get the url and parse it into a jsoup document
+            // Get the url and parse it into a jsoup document
             try{
                 System.out.println("this is my link"+ link);
                 URL aURL = new URL(link.toString());
                 Document doc = Jsoup.connect(link).get();
                 String domain = aURL.getHost();
-                //System.out.println("This is my forum domain " + domain);
 
                 Elements links = doc.select("a");
                 for (Element xlink : links) {
                     String absHref = xlink.attr("abs:href");
-                    //System.out.println(absHref);
                     if(!absHref.equals(""))
                     {
                         URL nURL = new URL(absHref.toString());
                         String newLink = nURL.getHost();
-                        //System.out.println("New link domain " + newLink);
                         if(newLink.equals(domain)) {
                             domainLinks.add(absHref);
                         }
@@ -94,10 +91,8 @@ public class Worker implements Runnable { //(added implements runnable)
                 // Remove all tags with script, style and hidden
                 doc.select("div.signature, .hidden, head, script").remove();
 
-                // Create a multimap to store the cleaned lines later with the word count as key and a list of the lines as value
-                HashMap<Integer, HashMap<Double, String>> wordCountedLines = new HashMap<Integer, HashMap<Double, String>>();
-                HashMap<Double, String> ttRatio_lines = new HashMap<Double, String>();
-
+                // Create a Hashmap to store the cleaned lines later with the word count as key and a list of the lines as value
+                HashMap<Integer, Ratio_Content_Pair> wordCountedLines = new HashMap<Integer, Ratio_Content_Pair>();
 
                 // Parse the document as html and delete all the newlines and useless whitespaces
                 String sourceCode=doc.html();
@@ -109,13 +104,16 @@ public class Worker implements Runnable { //(added implements runnable)
                 // Save the cleaned lines into a multimap by word count
                 int lineNumber=0;
                 double totalTTRatio=0;
-                int radius = 20;
+                int radius = 35;
                 for(String line:lines){
-                    //System.out.println(line);
-                    Pattern tags = Pattern.compile("<.*?>");
-                    Matcher tagMatch = tags.matcher(line);
+
                     int lineTagCounter = 0;
                     double lineWordCount = 0;
+                    double ttRatio = 0;
+
+                    Pattern tags = Pattern.compile("<.*?>");
+                    Matcher tagMatch = tags.matcher(line);
+
 
                     while (tagMatch.find())
                     {
@@ -131,93 +129,43 @@ public class Worker implements Runnable { //(added implements runnable)
 
                         lineNumber++;
                         if (lineTagCounter == 0) {
-                            if(!ttRatio_lines.containsValue(cleanLine)) {
-                                ttRatio_lines.put(lineWordCount, cleanLine);
-                                wordCountedLines.put(lineNumber, ttRatio_lines);
-                            }
+                                wordCountedLines.put(lineNumber, new Ratio_Content_Pair(lineWordCount, cleanLine));
+                                totalTTRatio = totalTTRatio + lineWordCount;
+
                         } else {
-                            if(!ttRatio_lines.containsValue(cleanLine)) {
-                                double ttRatio = lineWordCount / lineTagCounter;
-                                ttRatio_lines.put(ttRatio, cleanLine);
-                                wordCountedLines.put(lineNumber, ttRatio_lines);
+                                ttRatio = lineWordCount / lineTagCounter;
+                                wordCountedLines.put(lineNumber, new Ratio_Content_Pair(ttRatio, cleanLine));
                                 totalTTRatio = totalTTRatio + ttRatio;
-                            }
                         }
                         
                     }
                 }
-                // Get the average word count of the html document
-                /*
-                int averageWords=totalWordCount/lineNumber;
-                System.out.println(totalWordCount);
-                System.out.println(averageWords);
-                */
-
-
 
                 // For each loops to go through each line. After getting the line get the TTR of each line and if it
                 // above the threshold, get the value for that line (the content) and print it out with its TTR.
                 System.out.println("Lines above the threshold " + totalTTRatio/(2*radius+1) + "are: ");
-                //System.out.println(wordCountedLines.toString());
-
-                /*
-                for (Map.Entry<Integer, HashMap<Double, String>> entry : wordCountedLines.entrySet()) {
-                    HashMap<Double, String> innerMap = entry.getValue();
-                    System.out.println("Here! " + innerMap);
-                    for(Map.Entry<Double, String> innerEntry : innerMap.entrySet()) {
-                        if(innerEntry.getKey()>totalTTRatio/(2*radius+1))
-                        {
-                            //System.out.println("TTR: " + innerEntry.getKey() + ",  " + innerEntry.getValue());
-                        }
+               for (Map.Entry<Integer, Ratio_Content_Pair> entry : wordCountedLines.entrySet()) {
+                    Ratio_Content_Pair innerPair = entry.getValue();
+                    if (innerPair.ratio > totalTTRatio/(2*radius+1)){
+                        System.out.println("Ratio: " + innerPair.ratio + " Content: " + innerPair.content);
                     }
                 }
-                */
 
-
-                    HashMap<Double, String> innerMap = wordCountedLines.get(1);
-                    System.out.println("Here! " + innerMap);
-                    for(Map.Entry<Double, String> innerEntry : innerMap.entrySet()) {
-                        if(innerEntry.getKey()>totalTTRatio/(2*radius+1))
-                        {
-                            System.out.println("TTR: " + innerEntry.getKey() + ",  " + innerEntry.getValue());
-                        }
-                    }
-
-
-
-
-
-                /*
-                String value = (wordCountedLines.get(i));
-                System.out.println("Retreived value is : " + value);
-
-
-
-                System.out.println("The TTR is " + totalTTRatio);
-                System.out.println("The Total lines is " + wordCountedLines.size());
-
-                System.out.println("The mean TTR is " + (totalTTRatio/wordCountedLines.size()));
-
-                */
-
-                /*
                 //System.out.println("This is the link I am visiting right now "+link);
                 String text = link.replaceAll("https://","");
                 text = link.replaceAll("http://","");
                 text = text.replaceAll("/","*");
                 String ftext= text + ".txt";
                 FileWriter fw = new FileWriter(new File("collected_data", ftext));
-                for(int getWords=averageWords;getWords<=maxWordCount;getWords++) {
-                    Collection<String> myCollection = wordCountedLines.get(getWords);
-                    if (!wordCountedLines.get(getWords).isEmpty()) {
-                        if(myCollection.iterator().hasNext()) {
-                            fw.write(myCollection.toString());
-                            fw.write("\n");
-                        }
+                for (Map.Entry<Integer, Ratio_Content_Pair> entry : wordCountedLines.entrySet()) {
+                    Ratio_Content_Pair innerPair = entry.getValue();
+                    if (innerPair.ratio > totalTTRatio/(2*radius+1)){
+                        fw.write(innerPair.content);
+                        fw.write(System.getProperty( "line.separator" ));
                     }
                 }
                 fw.close();
-                */
+
 
                 // Print the elapsed time for the program to run
                 long stopTime=System.currentTimeMillis();
@@ -231,7 +179,6 @@ public class Worker implements Runnable { //(added implements runnable)
                     if(!dlink.equals("")){
                     if (!BloomFilter.bloom_filter_query(dlink,coffA,coffB)){
                         BloomFilter.bloom_filter_insert(dlink,coffA,coffB);
-                        //System.out.println("new links:"+dlink);
                         Main.bag_of_tasks.add(dlink);
                     }
                     }
@@ -250,6 +197,24 @@ public class Worker implements Runnable { //(added implements runnable)
             if (input.length()==0) count = 0;
             else count = input.split("\\s+").length;
             return count;
+        }
+
+
+        //Pair class for Ratio & Content pair, for each line
+        private class Ratio_Content_Pair{
+
+            public double ratio;
+            public String content;
+
+            public Ratio_Content_Pair(double ratio, String content){
+                this.ratio = ratio;
+                this.content = content;
+
+            }
+
+            public String toString(){
+                return  "{ "+ ratio + "," + content + " }";
+            }
         }
 
 
